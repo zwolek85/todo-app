b6fa062eeed10e196ce28be72994d4d908c57c43
DZ <zwolek@example.com>
2020-07-01 22:09:08 +0200

    Interceptory

---
+++ src/main/java/pl/home/david/todoapp/controller/LoggerInterceptor.java
@@ -0,0 +1,20 @@
+package pl.home.david.todoapp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class LoggerInterceptor implements HandlerInterceptor {
+    private static final Logger logger = LoggerFactory.getLogger(LoggerInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("[doInterceptor:] " + request.getMethod() + " " + request.getRequestURI());
+        return true;
+    }
+}
